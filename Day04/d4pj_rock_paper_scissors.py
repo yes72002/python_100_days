d2ceaea9e76bd9ad376875aba17342fe9bb0cc02
yes72002 <yes72002@gmail.com>
2024-05-01 17:28:15 +0800

    improve the coding style

--- Day04/d4pj_rock_paper_scissors.py
+++ Day04/d4pj_rock_paper_scissors.py
@@ -30,32 +30,32 @@ scissors = '''
 choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 images = [rock, paper, scissors]
 if choose >=3 or choose < 0:
-  print("You typed an invalid number, you lose!")
+    print("You typed an invalid number, you lose!")
 else:
-  print(images[choose])
+    print(images[choose])
 
-  computer_choose = random.randint(0, 2)
-  print("Computer choose: ")
-  print(images[computer_choose])
-  
-  if computer_choose == 0:
-    if choose == 0:
-      print("draw")
-    elif choose == 1:
-      print("You win")
+    computer_choose = random.randint(0, 2)
+    print("Computer choose: ")
+    print(images[computer_choose])
+
+    if computer_choose == 0:
+        if choose == 0:
+            print("draw")
+        elif choose == 1:
+            print("You win")
+        else:
+            print("You lose")
+    elif computer_choose == 1:
+        if choose == 0:
+            print("You lose")
+        elif choose == 1:
+            print("draw")
+        else:
+            print("You win")
     else:
-      print("You lose")
-  elif computer_choose == 1:
-    if choose == 0:
-      print("You lose")
-    elif choose == 1:
-      print("draw")
-    else:
-      print("You win")
-  else:
-    if choose == 0:
-      print("You win")
-    elif choose == 1:
-      print("You lose")
-    else:
-      print("draw")
+        if choose == 0:
+            print("You win")
+        elif choose == 1:
+            print("You lose")
+        else:
+            print("draw")
