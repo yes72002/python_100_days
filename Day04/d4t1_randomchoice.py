a5d5a5eca3372738574713380e7d157521390161
yes72002 <yes72002@gmail.com>
2024-05-01 17:27:16 +0800

    improve the coding style

--- Day04/d4t1_randomchoice.py
+++ Day04/d4t1_randomchoice.py
@@ -2,10 +2,9 @@
 # https://www.askpython.com/python-modules/python-random-module-generate-random-numbers-sequences
 # from 2. random.choice()
 import random
- 
+
 a = ['one', 'eleven', 'twelve', 'five', 'six', 'ten']
- 
 print(a)
- 
+
 for i in range(5):
     print(random.choice(a))
