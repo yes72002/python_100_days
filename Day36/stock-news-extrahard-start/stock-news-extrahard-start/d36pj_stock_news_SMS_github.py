5280abbe0624636d5676734a71b77f8e561880ad
yes72002 <yes72002@gmail.com>
2024-05-06 21:52:32 +0800

    better coding style

--- Day36/stock-news-extrahard-start/stock-news-extrahard-start/d36pj_stock_news_SMS_github.py
+++ Day36/stock-news-extrahard-start/stock-news-extrahard-start/d36pj_stock_news_SMS_github.py
@@ -5,20 +5,20 @@
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 ## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 ## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# Send a seperate message with the percentage change and each article's title and description to your phone number.
 
 
-#Optional: Format the SMS message like this: 
+#Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 or
 "TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
 
@@ -118,11 +118,10 @@ for i in range(0, 3):
     print(message_body)
 
     message = client.messages \
-                    .create(
-                        # body=message_body,
-                        body="test",
-                        from_=VIRTUAL_TWILIO_NUMBER,
-                        to=VERIFIED_NUMBER
-                    )
-                    
+        .create(
+            # body=message_body,
+            body="test",
+            from_=VIRTUAL_TWILIO_NUMBER,
+            to=VERIFIED_NUMBER
+        )
     print(message.status)
